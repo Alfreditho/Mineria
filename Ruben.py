8fd840da361aece2c3b496b729135cf1a06de01a
rubent2004 <rubentorressanchez204@gmail.com>
2025-03-01 04:10:00 +0000

    BTS-7 commit Ruben Torres

---
+++ Ruben.py
@@ -0,0 +1,7 @@
+#prueba de commit
+# Description: This program will calculate the area of a circle
+# given the radius. The program will also calculate the volume
+# of a sphere given the radius.
+# The program will also calculate the area of a rectangle given the
+# length and width.
+# The program will also calculate the area of a triangle given the base
